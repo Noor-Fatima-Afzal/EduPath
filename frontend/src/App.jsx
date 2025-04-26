8abafd10ad5d2abbb765ecf3f29635f59faa9526
Trevor5008 <trezrevner@icloud.com>
2025-04-25 22:34:40 -0400

    [frontend] Scaffolded frontend env w/ tailwind, vite and postcss setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-600">EduPath</h1>
+    </div>
+  )
+}
+
+export default App
