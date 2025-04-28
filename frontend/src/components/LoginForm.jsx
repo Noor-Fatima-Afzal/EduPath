d382633340eaa5863b4a04f1c3f82f10445f548b
Trevor5008 <trezrevner@icloud.com>
2025-04-28 01:19:51 -0400

    Cleaned up formatting

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -9,70 +9,70 @@ function LoginForm(
 
     return (
     	<form 
-	    action="#" 
-	    method="POST" 
-	    className="space-y-6" 
-	    onSubmit={handleFormSubmit}
-	>
-	    <div>
-		<label htmlFor="username" className="block text-sm/6 font-medium text-white">
+        action="#" 
+        method="POST" 
+        className="space-y-6" 
+        onSubmit={handleFormSubmit}
+	    >
+	      <div>
+		      <label htmlFor="username" className="block text-sm/6 font-medium text-white">
 	    	    Username
 	        </label>
-	    	<div className="mt-2">
+	    	  <div className="mt-2">
 	    	    <input 
 	    	        id="username" 
-	                name="username"
+	              name="username"
 	    	        type="text" 
 	    	        value={userName || ""}
 	    	        onChange={handleUserNameChange}
 	    	        className={`block w-full rounded-t-md bg-white px-3 py-1.5 text-base 
-	    		   text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 
-			   placeholder:text-gray-400 focus:relative focus-outline-2 focus:outline-2 
-			   focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6`}
-	    		aria-label="Username"
-	    		required
-		    />
+	    		          text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 
+			              placeholder:text-gray-400 focus:relative focus-outline-2 focus:outline-2 
+			              focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6`}
+	    		      aria-label="Username"
+	    		      required
+		        />
 	        </div>
-	    </div>
-	    <div>
-	    	<div className="flex items-center justify-between">
-		    <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+	      </div>
+	      <div>
+	    	  <div className="flex items-center justify-between">
+		        <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
 	    	        Password
-	            </label> 
-		<div className="text-sm">
-	    	    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-	    		Forgot password?
-	    	    </a>
-	 	</div>
-	    </div>
-	    	<div className="mt-2">
+	          </label> 
+		        <div className="text-sm">
+	    	      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+	    		      Forgot password?
+	    	      </a>
+	 	        </div>
+	        </div>
+	    	  <div className="mt-2">
 	    	    <input 
 	    	        id="password" 
-	                name="password"
+	              name="password"
 	    	        type="password" 
 	    	        value={passwd || ""}
 	    	        onChange={handlePasswordChange}
 	    	        className={`block w-full rounded-md bg-white px-3 py-1.5 text-base 
-	    		   text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 
-			   placeholder:text-gray-400 focus:outline focus:outline-2 
-			   focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6`}
-	    		aria-label="Password"
-	    		required
-		    />
-	   	</div>
-	    </div>
+	    		          text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 
+			              placeholder:text-gray-400 focus:outline focus:outline-2 
+			              focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6`}
+	    		      aria-label="Password"
+	    		      required
+		        />
+	   	    </div>
+	      </div>
 	    <div>
 	    	<button 
 	    	    type="submit"
 	    	    className={`flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5
-		    	text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
-			focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+		    	    text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
+			        focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
 	    	>
-		    Sign in
+		      Sign in
 	    	</button>
 	    </div>
 	</form>
-    );
+  );
 }
 
 export default LoginForm
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -14,11 +14,11 @@ function LoginPage() {
 
     // Notification for login attempt
     const showToast = (type, message) => {
-	setToast({ type, message })
-	// Temporary notification
-	window.setTimeout(() => {
-	   setToast(null);
-	}, 3000); // 3-sec duration
+	    setToast({ type, message })
+	    // Temporary notification
+	    window.setTimeout(() => {
+	      setToast(null);
+	    }, 3000); // 3-sec duration
     } 
 
     // Updates username entry
@@ -33,58 +33,57 @@ function LoginPage() {
 	
     // Submits form, triggers toast notification
     const handleFormSubmit = async (evt) => {
-	evt.preventDefault()
-	console.log("submitting")
-	setLoading(true) // TODO Loading state flag
-	// Handle successfull vs. failed login    
-    	try {
-	    const result = await mockLogin(userName, password);
-	    showToast("success", "Login successful!");
-	    localStorage.setItem('token', result.token);
-	    // navigate("/recommendations"); // TODO add page/route
-	} catch (err) {
-	    showToast("error", err.message || "Login failed!");
-	} finally {
-	    setLoading(false);
-	}
+      evt.preventDefault()
+      console.log("submitting")
+      setLoading(true) // TODO Loading state flag
+      // Handle successfull vs. failed login    
+        try {
+          const result = await mockLogin(userName, password);
+          showToast("success", "Login successful!");
+          localStorage.setItem('token', result.token);
+          // navigate("/recommendations"); // TODO add page/route
+        } catch (err) {
+            showToast("error", err.message || "Login failed!");
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
-	<>
-	    {toast && (
-		<Alert alertType={toast.type} alertMessage={toast.message} />
-	    )}
-	    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-	        <img
-	    	   alt="< EduPath logo >"
-	           src=""
-	           className="mx-auto h-10 w-auto"
-	        />
-	    	<h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-	    		Sign into your account
-	    	</h2>
-	    </div>
-	    {/* Login Form */}
-	    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-	        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-	    	    <LoginForm 
-	    		userName={userName} 
-	    		passwd={password}
-			handleUserNameChange={handleUserNameChange}
-	    		handlePasswordChange={handlePasswordChange}
-	    		handleFormSubmit={handleFormSubmit}
-	    	    />
-	    	   <p className="mt-10 text-center text-sm/6 text-gray-500">
-	    		Not a member?{' '}
-	    		<a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-	    		    Start a 14-day trial
-	    		</a>
-	    	   </p>
-	    	</div>
-	    </div>
-	</>
+	    <>
+        {toast && (
+          <Alert alertType={toast.type} alertMessage={toast.message} />
+        )}
+        <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <img
+             alt="< EduPath logo >"
+               src=""
+               className="mx-auto h-10 w-auto"
+            />
+          <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign into your account
+          </h2>
+        </div>
+	      {/* Login Form */}
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+            <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+              <LoginForm 
+                userName={userName} 
+                passwd={password}
+                handleUserNameChange={handleUserNameChange}
+                handlePasswordChange={handlePasswordChange}
+                handleFormSubmit={handleFormSubmit}
+	    	      />
+             <p className="mt-10 text-center text-sm/6 text-gray-500">
+                Not a member?{' '}
+                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Start a 14-day trial
+                </a>
+             </p>
+	    	  </div>
+	      </div>
+	    </>
     );
-
 }
 
 export default LoginPage;
